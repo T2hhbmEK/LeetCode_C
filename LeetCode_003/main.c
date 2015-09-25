7fb8e1c580899c52777fee71b234eba17de4e5f9
crucifyml <crucify_m_l@163.com>
2015-09-26 00:37:53 +0800

    Test code

--- LeetCode_003/main.c
+++ LeetCode_003/main.c
@@ -1,6 +1,20 @@
 /**
-* Given a string, find the length of the longest substring without repeating 
-* characters. For example, the longest substring without repeating letters for 
-* "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring 
+* Given a string, find the length of the longest substring without repeating
+* characters. For example, the longest substring without repeating letters for
+* "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring
 * is "b", with the length of 1.
 */
+
+#include<stdio.h>
+
+int lengthOfLongestSubstring(char* s) {
+
+}
+
+int main() {
+	char s[] = "abcabcbb";
+	int r;
+	r = lengthOfLongestSubstring(s);
+	printf("%d\n", r);
+	return;
+}
